6c830741315ca89bb8eaa02cf8c8f4046b4b3f23
theo <117802776+theaji@users.noreply.github.com>
2023-01-12 23:22:48 -0500

    added string_split

---
+++ Python/string_split.py
@@ -0,0 +1,11 @@
+#Extract DNS sequences from a string and transform them into RNA by replacing the nucleotides
+
+seq = "tatgctttcc#tataggtctg#ctattcaatg"
+dna_list = seq.split("#")
+print(f"The DNA list is: {dna_list} \n")
+print("Results: \n")
+
+for dna in dna_list:
+    rna = dna.replace("t","u")
+    print(f"DNA: {dna} -> RNA: {rna}")
+
