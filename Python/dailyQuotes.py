01343304fd591431bff6c2510f5e1c6481a91cef
theo <117802776+theaji@users.noreply.github.com>
2023-01-18 00:03:57 -0500

    added dailyQuotes

---
+++ Python/dailyQuotes.py
@@ -0,0 +1,28 @@
+import requests
+
+r = requests.get("https://quotes.rest/qod?language=en", timeout=10)
+
+
+#Convert into json
+response = r.json()
+
+#Check for successful quote retrieval
+
+def retrieve():
+
+    if "success" in response: 
+        print("Your Quote of the Day is: \n")
+        reply()
+
+    else:
+        print("There was an issue retrieving quote!!")
+
+#Print response
+
+def reply():
+    quote = response["contents"]["quotes"][0]["quote"]
+    author = response["contents"]["quotes"][0]["author"]
+    print(quote)
+    print("- " + author)
+
+retrieve()
