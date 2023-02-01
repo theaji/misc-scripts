78ec4b2e296cd32f6aabf5ece3c3cc7f4257a200
theo <117802776+theaji@users.noreply.github.com>
2023-02-01 05:28:32 -0500

    merged branch

---
+++ Python/pytest/main.py
@@ -0,0 +1,16 @@
+#/usr/bin/python3
+
+#Sum of two numbers
+
+def calc_total(a,b):
+    return a+b
+
+#Multiplication of two numbers
+
+def calc_multiply(a,b):
+    return a*b
+
+print("The sum of your calculation is: " + str(calc_total(2,1)))
+print("The value of your calculation is: " + str(calc_multiply(3,2)))
+
+
---
+++ Python/pytest/test_main.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+
+import main
+
+def test_calc_total():
+    total = main.calc_total(4,5)
+    assert total == 9
+
+def test_calc_multiply():
+    result = main.calc_multiply(10,3)
+    assert result == 30
