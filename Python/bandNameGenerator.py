c5741c974b7d0910fe4d1cedf90c2da52e951d97
theo <117802776+theaji@users.noreply.github.com>
2023-02-08 21:26:41 -0500

    cleaned up bandNameGenerator

--- Python/bandNameGenerator.py
+++ Python/bandNameGenerator.py
@@ -2,13 +2,14 @@
 
 print("Welcome to the Band Name Generator!!! \n\n")
 
-#Ask the user for input
+# Ask the user for input
+
 
 def play_game():
-    
     global name
     name = input("Please enter your first name:\n")
-    #Check if name starts with an alphabet and length does not exceed 20 characters
+    """ Check if name starts with an alphabet
+    and length does not exceed 20 characters"""
     if not name[0].isalpha() or len(name) >= 20:
         print("Please enter a valid name ")
         main()
@@ -20,17 +21,19 @@ def play_game():
         else:
             print("Your recommended band name is: " + name + " " + color)
 
+
 def play_again():
-    again = input("Would you like to play again? Please type 'yes' or 'no' \n") .lower()
+    again = input("Would you like to play again? Please type 'yes' or 'no' \n").lower()
     while again != "yes":
         print("Thanks for playing " + name + ". See you next time!")
         break
     else:
         main()
-        
+
+
 def main():
     play_game()
     play_again()
-    
-    
+
+
 main()
