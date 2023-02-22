c1b288b4434e3a02da7bf6fb371c491e44670b7c
theo <117802776+theaji@users.noreply.github.com>
2023-02-21 23:26:13 -0500

    added tf-ec2_simple

---
+++ Terraform/ec2_simple/README.md
@@ -0,0 +1,19 @@
+# Objective
+
+Create a single EC2 instance in the default AWS VPC
+
+## Services covered
+
+EC2 | VPC
+
+## Commands used
+
+Initialize project: `terraform init`
+
+Validate the configuration: `terraform validate`
+
+Apply consistent formatting: `terraform fmt`
+
+Create resources: `terraform apply`
+
+Delete resources: `terraform destroy`
--- Terraform/ec2_simple/main.tf
+++ Terraform/ec2_simple/main.tf
@@ -1,23 +1,25 @@
-terraform { 
+terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.54.0"
     }
-  
+
   }
 
 }
 
 provider "aws" {
-    region = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
+
 }
 
-resource "aws_instance" "test_server" {
-  ami     = "ami-0fe472d8a85bc7b0e"
+resource "aws_instance" "testing_server" {
+  ami           = "ami-0fe472d8a85bc7b0e"
   instance_type = "t2.micro"
-  
+
   tags = {
     Name = "tf-server"
-    }
+  }
 }
