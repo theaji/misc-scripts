2246b7736e82818f56d783ec6c131f75b59d1d47
theo <117802776+theaji@users.noreply.github.com>
2023-06-02 00:40:48 -0400

    modfied password_gen

--- Misc/password_gen.sh
+++ Misc/password_gen.sh
@@ -2,19 +2,22 @@
 # Author: Theo A
 # Date: 02/02/23
 # Description: Script to generate a random password 
-# Modified: 05/15/23
+# Modified date/reason: 06/02/23 - changed max password length from 100 to 50
 
 echo
 #Prompt user for how many characters to include in password and store in variable "input"
-read -rp "How many characters would you like your password to be? : " input
+read -rp "How many characters would you like your password to be?: " input
 
-#Only accept input between 8-100
-if [ "$input" -ge "8" ] && [ "$input" -le "100" ] 2> /dev/null; then
+#Only accept input between 8-50
+if [ "$input" -ge "8" ] && [ "$input" -le "50" ] 2> /dev/null; then
   randompw=$(tr -dc 'A-Za-z0-9!"#$%&\()*+,-./:;<=>?@[\]^_{|}~' </dev/urandom | head -c "$input")
+  echo
   echo "Your randomly generated password is: "
+  echo
   echo "$randompw"
 else
-  echo "Please enter a number between 8 and 100"
+  echo
+  echo "ERROR! Please enter a number between 8 and 50"
 fi
 echo
 
