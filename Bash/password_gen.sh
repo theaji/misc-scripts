c1f13169de4369ecb3f8520d6de0640b09429684
theo <117802776+theaji@users.noreply.github.com>
2023-09-20 05:20:47 -0400

    added help function

--- Bash/password_gen.sh
+++ Bash/password_gen.sh
@@ -1,23 +1,109 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 # Author: Theo A
-# Date: 02/02/23
-# Description: Script to generate a random password 
-# Modified date/reason: 06/02/23 - changed max password length from 100 to 50
+# Description: Generate a random password
+# Modified date/reason: 6/2/23 - added help function
 
-echo
-#Prompt user for how many characters to include in password and store in variable "input"
-read -rp "How many characters would you like your password to be?: " input
+######################
+# Initialize variables
+######################
 
+EXIT_ON_ERROR="true"
+PASSWD_LENGTH=""
+
+#########################################
+# help - used to provide usage guidance
+#########################################
+
+help() {
+  echo
+  echo "Supported cli arguments:"
+  echo -e "\t[-h] ->> print this help (optional)"
+  echo
+  echo -e "\t[-d] ->> turn on debug mode (optional)"
+  echo
+  echo -e "\t[-p] ->> specify password length (required)"
+  echo
+  echo "USAGE: $0 -p 12"
+  echo
+}
+
+
+########################################
+# debug: set debug for troubleshooting
+########################################
+debug() {
+  set -x
+}
+
+### Set to true to exit if a non-zero exit status is encountered
+if [ "${EXIT_ON_ERROR}" == "true" ]; then
+  set -e
+fi
+
+###########################################
+# generatepassword - displays new password
+###########################################
+
+
+GeneratePassword() {
 #Only accept input between 8-50
-if [ "$input" -ge "8" ] && [ "$input" -le "50" ] 2> /dev/null; then
-  randompw=$(tr -dc 'A-Za-z0-9!"#$%&\()*+,-./:;<=>?@[\]^_{|}~' </dev/urandom | head -c "$input")
+
+if [ "$PASSWD_LENGTH" -ge "8" ] && [ "$PASSWD_LENGTH" -le "50" ] 2> /dev/null; then
+
+  randompw=$(tr -dc 'A-Za-z0-9!"#$%&\()*+,-./:;<=>?@[\]^_{|}~' </dev/urandom | head -c "$PASSWD_LENGTH")
+
   echo
   echo "Your randomly generated password is: "
   echo
   echo "$randompw"
+
+
 else
+
   echo
   echo "ERROR! Please enter a number between 8 and 50"
+  help
+
 fi
+
 echo
 
+}
+
+
+#############################
+# Process input variables
+############################
+
+while getopts ":hdp:" opt; do
+	case "${opt}" in
+    	# display help
+    	h)
+      	help # call help function
+      	exit 0
+      	;;
+   	 d)
+      	debug
+   	   ;;
+    	p)
+      	PASSWD_LENGTH="${OPTARG}";;
+   	*) # incorrect option
+       	echo "Error: Invalid options"
+       	help
+       	exit 1
+       	;;
+	esac
+done
+
+###############
+# BEGIN PROGRAM
+###############
+
+
+if [[ "$#" -gt 2 ]]; then
+    	help
+elif [[ "$#" -lt 2 ]]; then
+    	help
+else
+    	GeneratePassword
+fi
